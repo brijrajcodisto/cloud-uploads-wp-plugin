7f0565a7cb63d841d49ffff8709d2e57ebfd4160
brijrajcodisto <brijraj2011@zohomail.in>
2024-12-08 17:47:12 +0530

    changes

--- inc/class-cloud-uploads-api-handler.php
+++ inc/class-cloud-uploads-api-handler.php
@@ -16,7 +16,7 @@ class Cloud_Uploads_Api_Handler {
 	 *ś
 	 * @var string (URL)
 	 */
-	public $server_root = 'http://localhost:3000/';
+	public $server_root = 'http://127.0.0.1:3000/';
 
 	/**
 	 * Path to the REST API on the server.
