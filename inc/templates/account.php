6228d029067b4d7a50c15dad41dabcfb24fb51ee
brijrajcodisto <brijraj2011@zohomail.in>
2024-12-09 14:17:01 +0530

    changes

--- inc/templates/account.php
+++ inc/templates/account.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
             <span class="h2 text-nowrap"><?php echo esc_html__($this->size_format_zero( $cloud_size, 2 )); ?><small class="text-muted"> / <?php echo esc_html__(number_format_i18n( $cloud_files )); ?></small></span>
 
             <div class="container p-0 ml-md-3">
-              <?php foreach ( $this->iup_instance->get_filetypes( false, $api_data->stats->site->types ) as $type ) { ?>
+              <?php foreach ( $this->cup_instance->get_filetypes( false, $api_data->stats->site->types ) as $type ) { ?>
                 <div class="row mt-2">
                   <div class="col-1"><span class="badge badge-pill" style="background-color: <?php echo esc_html__($type->color); ?>">&nbsp;</span></div>
                   <div class="col-4 lead text-nowrap"><?php echo esc_html__($type->label); ?></div>
