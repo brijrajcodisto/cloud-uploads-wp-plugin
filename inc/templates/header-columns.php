830dfbe94a4a468acde38108da267987833c1ef3
brijrajcodisto <brijraj2011@zohomail.in>
2024-12-20 21:43:11 +0530

    chan

--- inc/templates/header-columns.php
+++ inc/templates/header-columns.php
@@ -10,12 +10,12 @@
 	</div>
 	<div class="col-12 col-lg-6 mb-4">
 		<?php
-		if ( $this->video->is_video_active() ) {
-			$video_library_settings = $this->video->get_library_settings();
-			require_once( dirname( __FILE__ ) . '/video-overview.php' );
-		} else {
-			require_once( dirname( __FILE__ ) . '/video-disabled.php' );
-		}
+		// if ( $this->video->is_video_active() ) {
+		// 	$video_library_settings = $this->video->get_library_settings();
+		// 	require_once( dirname( __FILE__ ) . '/video-overview.php' );
+		// } else {
+		// 	require_once( dirname( __FILE__ ) . '/video-disabled.php' );
+		// }
 		?>
 	</div>
 </div>
