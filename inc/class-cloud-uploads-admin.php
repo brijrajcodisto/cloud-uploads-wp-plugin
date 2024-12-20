3d38b7a69563aa9cb9c38f2c1cc4d97f9b51f331
brijrajcodisto <brijraj2011@zohomail.in>
2024-12-20 09:27:00 +0530

    changes

--- inc/class-cloud-uploads-admin.php
+++ inc/class-cloud-uploads-admin.php
@@ -555,7 +555,7 @@ class Cloud_Uploads_Admin {
 					$file = $to_sync_files[$i];
 					$data = array("url"=>'https://wp.test.mackshost.com/wp-content/uploads'.$file);
 					$result = $api->call('api/file', $data, 'POST');
-					//error_log( print_r( $to_sync, true ) );
+					error_log( print_r( $result, true ) );
 					error_log( print_r( $file, true ) );
 					error_log( print_r( $data, true ) );
 				}
